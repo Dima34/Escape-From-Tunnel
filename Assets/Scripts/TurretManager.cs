5708a0cd2e46e1e06469410aa99c7e41cd8ab026
Dima34 <vavilov9823@gmail.com>
2022-06-26 21:16:03 +0300

    need to make smooth turret move

--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class TurretManager : MonoBehaviour
 {
+    [SerializeField] TurretZoneEvent turretZoneScript;
     [SerializeField] GameObject TurretZone;
     [SerializeField] GameObject TurretBase;
     [SerializeField] float zoneRadius = 1.5f;
-    [SerializeField] TurretZoneEvent turretZoneScript;
+    [SerializeField] float degreesPerSecond = 20f;
 
     Ray ray;
 
@@ -35,34 +36,19 @@ public class TurretManager : MonoBehaviour
         Collider colliderInfo = (obj as Collider);
 
         if(colliderInfo == null) {return;}
+        // Draw an up vector of turret
+        Debug.DrawRay(TurretBase.transform.position, TurretBase.transform.forward * 5f, Color.red);
 
-        Transform contactorTransform = colliderInfo.gameObject.transform;
-        Vector3 direction = TurretBase.transform.position - contactorTransform.position;
+        Vector3 contactorVec = colliderInfo.transform.position - TurretBase.transform.position;
+        contactorVec.Normalize();
 
-        // TurretBase.transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        TurretBase.transform.rotation = Quaternion.Lerp(TurretBase.transform.rotation, Quaternion.Euler(direction), 2f);
+        // Draw a toContactor ray
+        Debug.DrawRay(TurretBase.transform.position, contactorVec * 5f, Color.green);
 
-        // Draw ray when rocket is in collision
-        ray = new Ray();
-        ray.origin = TurretBase.transform.position;
-        ray.direction = TurretBase.transform.forward;
         
-        RaycastHit hit;
 
-        bool isRocketOnWay = Physics.Raycast(ray,out hit, zoneRadius);
-
-        // Visualize the ray
-        Color RayColor;
-
-        if(isRocketOnWay){
-            RayColor = Color.green;
-        }else{
-            RayColor = Color.red;
-        }
-
-        Debug.DrawRay(TurretBase.transform.position, TurretBase.transform.forward*(-10f), RayColor);
-    }   
-
+        TurretBase.transform.rotation = Quaternion.FromToRotation(Vector3.forward, contactorVec * Time.deltaTime / degreesPerSecond);
+    }
 
     // Editor radius displaying
     void OnDrawGizmosSelected() {
