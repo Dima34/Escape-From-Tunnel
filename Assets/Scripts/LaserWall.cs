774d7df961885d8b558a61a862005702b5477318
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-07-01 01:39:12 +0300

    Laser sound added

--- Assets/Scripts/LaserWall.cs
+++ Assets/Scripts/LaserWall.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class LaserWall : MonoBehaviour
 {
     [SerializeField] GameObject LaserRay;
+    [SerializeField] AudioClip LaserSound;
+    [SerializeField] AudioSource LaserAudioSource;
     [SerializeField] float BetweenDelay = 6f;
     [SerializeField] float ActiveTime = 4f;
 
@@ -12,11 +14,12 @@ public class LaserWall : MonoBehaviour
     Vector3 defaultLaserSize;
     Vector3 defaultLaserPos;
 
+
     private void Awake()
     {
         defaultLaserSize = LaserRay.transform.localScale;
         defaultLaserPos = LaserRay.transform.position;
-        
+
         TurnLaserOn();
     }
 
@@ -25,28 +28,21 @@ public class LaserWall : MonoBehaviour
         Debug.Log("Laser is ON");
 
         Ray distanceRay = new Ray(LaserRay.transform.position, Vector3.right);
-        Debug.DrawRay(LaserRay.transform.position, Vector3.right * 100f,Color.red, ActiveTime);
+        Debug.DrawRay(LaserRay.transform.position, Vector3.right * 100f, Color.red, ActiveTime);
 
         RaycastHit[] distanceRayHits = Physics.RaycastAll(distanceRay);
 
         // Check where is an obstacle to draw an laser line nessasary length
-        if (distanceRayHits != null){
-            foreach(RaycastHit hit in distanceRayHits)
+        if (distanceRayHits != null)
+        {
+            foreach (RaycastHit hit in distanceRayHits)
             {
                 string hitTag = hit.collider.tag;
 
                 if (hitTag == "Obstacle")
                 {
-                    // Get distance to ray collided object
-                    float distToObstacle = hit.distance;
-
-                    print("Dist to obstacle - " + distToObstacle);
-
-                    // Set laser length to ray length
-                    LaserRay.transform.localScale = new Vector3(distToObstacle, LaserRay.transform.localScale.y, LaserRay.transform.localScale.z);
-
-                    // Set position to center of ray
-                    LaserRay.transform.position = LaserRay.transform.position + LaserRay.transform.right * (distToObstacle / 2);
+                    createLaserLine(hit.distance);
+                    turnLaserSoundOn();
                 }
             }
         }
@@ -56,14 +52,35 @@ public class LaserWall : MonoBehaviour
 
     void TurnLaserOff()
     {
-        Debug.Log("Laser is OFF");
-
-        // set default position and scale
-        LaserRay.transform.localScale = defaultLaserSize;
-        LaserRay.transform.position = defaultLaserPos;
+        resetLaserLine();
+        turnLaserSoundOff();
 
         Invoke("TurnLaserOn", BetweenDelay);
     }
 
-    
+    void createLaserLine(float distToObstacle)
+    {
+        // Set laser length to ray length
+        LaserRay.transform.localScale = new Vector3(distToObstacle, LaserRay.transform.localScale.y, LaserRay.transform.localScale.z);
+
+        // Set position to center of ray
+        LaserRay.transform.position = LaserRay.transform.position + LaserRay.transform.right * (distToObstacle / 2);
+    }
+
+    void resetLaserLine(){
+        LaserRay.transform.localScale = defaultLaserSize;
+        LaserRay.transform.position = defaultLaserPos;
+    }
+
+
+    void turnLaserSoundOn()
+    {
+        LaserAudioSource.clip = LaserSound;
+        LaserAudioSource.Play();
+    }
+
+    void turnLaserSoundOff(){
+        LaserAudioSource.Stop();
+    }
+
 }
