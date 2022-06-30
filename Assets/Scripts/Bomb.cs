1a7bfa1b5b56479d78a7ef40fb18cb549197a7b5
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-07-01 02:19:50 +0300

    Bomb fly sound added

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,9 +6,15 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] GameObject afterCollisionObject;
 
+    private void Awake() {
+        GetComponent<AudioSource>().Play();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(afterCollisionObject, transform.position, transform.rotation);
         Destroy(this.gameObject);
     }
+
+    
 }
